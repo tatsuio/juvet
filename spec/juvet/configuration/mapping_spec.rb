751aeeee4b0fa847263cf56e2ed7733a92fd1453
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-15 19:33:01 -0500

    Pass in the adapter config to the mapping config

--- spec/juvet/configuration/mapping_spec.rb
+++ spec/juvet/configuration/mapping_spec.rb
@@ -6,10 +6,11 @@ describe Juvet::Configuration::Mapping do
         end
       }
     end
+    let(:adapter) { Juvet::Configuration::Adapter.new :null }
     subject { described_class.new &blk }
 
     it "creates a mapper from a block" do
-      result = subject.build
+      result = subject.build adapter
 
       expect(result).to be_an_instance_of Juvet::Mapper
       expect(result.collections.length).to eq 1

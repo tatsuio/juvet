c15dfecf5a5eb47e5cea95ebd9783b4fc7f18232
Jamie Wright <jamie@brilliantfantastic.com>
2016-02-16 20:55:43 -0500

    Implement all which returns just an empty array

--- spec/juvet/adapters/null_adapter_spec.rb
+++ spec/juvet/adapters/null_adapter_spec.rb
@@ -2,6 +2,12 @@ describe Juvet::Adapters::NullAdapter do
   let(:entity) { Object.new }
   subject { described_class.new }
 
+  describe "#all" do
+    it "simply returns an empty array" do
+      expect(subject.all).to be_empty
+    end
+  end
+
   describe "#create" do
     it "simply returns the entity" do
       expect(subject.create(entity)).to eq entity
